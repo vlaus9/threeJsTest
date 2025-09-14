9c130ec09ea5fbf8fcb15c474051928ccfe01d6d
Vlaus9 <poroxnenko_official@bk.ru>
2025-09-14 12:33:01 +0300

    Исправил цвет, добавил текст в header

--- src/DraggingWindow.jsx
+++ src/DraggingWindow.jsx
@@ -5,7 +5,9 @@ export default function dragginWindow () {
     return (
         <>
             <div className='draggingWindow'>
-                <div className='header-draggingWindow'></div>
+                <div className='header-draggingWindow'>
+                    <p className='header-draggingWindow-name'>Название окна</p>
+                </div>
             </div>
         </>
     )
