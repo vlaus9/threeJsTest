7f49606496803457b20662b66d3aab2edb529bcc
Vlaus9 <poroxnenko_official@bk.ru>
2025-09-16 00:09:03 +0300

    Добавить блок тела draggingWindow

--- src/DraggingWindow.jsx
+++ src/DraggingWindow.jsx
@@ -8,6 +8,10 @@ export default function dragginWindow () {
                 <div className='header-draggingWindow'>
                     <p className='header-draggingWindow-name'>Название окна</p>
                 </div>
+                <div className='draggingWindow-body'>
+                    <p className='draggingWindow-body-text'>Какой то текст</p>
+                </div>
+                
             </div>
         </>
     )
